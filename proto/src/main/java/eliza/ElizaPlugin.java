d6887620b901b80daeebc2ce5cd8fc145007328b
basilboli <basilboli@gmail.com>
2011-11-05 16:20:14 +0100

    java kickoff

---
+++ proto/src/main/java/eliza/ElizaPlugin.java
@@ -0,0 +1,17 @@
+package eliza;
+/**
+ * User: basilboli
+ * Date: 05/11/11
+ * Time: 16:13
+ */
+public class ElizaPlugin {
+
+//    public static ElizaProcessor INSTANCE = new ElizaProcessor();
+//    static String scriptPathname = "script";
+//    static final boolean local = true;
+//    public static String getReponse(String phrase) {
+//        ElizaMain eliza = new ElizaMain();
+//        eliza.readScript(local,scriptPathname);
+//        return eliza.processInput(phrase);
+//    }
+}
