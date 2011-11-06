c0baec8393df111ab34be56a3cd65085c41df3f0
basilboli <basilboli@gmail.com>
2011-11-06 06:17:02 +0100

    finally stackoverflow. best question for a given query

--- proto/src/main/java/StackOverflowPlugin.java
+++ proto/src/main/java/StackOverflowPlugin.java
@@ -1,30 +1,38 @@
 import bender.plugin.SuperSimplePluginForJava;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.zip.GZIPInputStream;
 
 /**
  * User: basilboli
  * Date: 05/11/11
+ *
+ * Using stackoverflow api (in this case /search to get the most scored question with the best
+ * answer for a given query)
+ *
  */
 public class StackOverflowPlugin extends SuperSimplePluginForJava{
 
-    private static String API_KEY= "QiEaCg69wU2WhNPmcVNO_g";
+    private static String API_KEY= "QiEaCg69wU2WhNPmcVNO_g";//unique key for this app to use stack api
+
     private static String API_PREFIX="http://api.stackoverflow.com/1.1/";
     private static String SEARCH_QUESTIONS_URL = API_PREFIX+"search?intitle={query}&key="+API_KEY;
     private static String SHOW_QUESTION_URL = API_PREFIX+"questions/{id}?body=true&key="+API_KEY;
     private static String SHOW_ANSWERS_URL = API_PREFIX+"questions/{id}/answers?body=true&key="+API_KEY;
     private static int RESULTNO = 1;
-    private static int PAGESIZE = 30;
-    final JsonParser parser = new JsonParser();
-
+    private static int PAGESIZE = 10;
 
     public String name() {
         return "Stackoverflow Plugin";
@@ -32,48 +40,100 @@ public class StackOverflowPlugin extends SuperSimplePluginForJava{
 
      public String process(String s){
 
-           if(s.equals("Bonjour !")) {
-//                 HttpGet reponse = new HttpGet(API_PREFIX+"users/633980/questions?key="+API_KEY);
-//                 System.out.println(reponse.toString());
-               try {
-                   HttpClient httpclient = new DefaultHttpClient();
+         if (s.contains("stackoverflow")) {
+            String url = SEARCH_QUESTIONS_URL.replace("{query}",
+                     s.replace("stackoverflow", "").replace(" ", "%20"));
 
-                   HttpGet request = new HttpGet(API_PREFIX+"users/633980/questions?key="+API_KEY);
-                   request.addHeader("Accept-Encoding", "gzip");
-                   HttpResponse response = httpclient.execute(request);
-                   HttpEntity entity = response.getEntity();
-                   if (entity != null) {
-                        long len = entity.getContentLength();
-                        if (len != -1 && len < 2048) {
-                            System.out.println(EntityUtils.toString(entity));
-                        }
-                   }
-               } catch (IOException e) {
-                   e.printStackTrace();
-                 }
-               }
-         return s;
+             JsonElement questions = fetch(url).get("questions");
+             if (questions.isJsonArray()) {//if we have several questions for the query
+                 JsonArray array = questions.getAsJsonArray();
+                 if (array.size() == 0)
+                     return "i don't know";
+
+                 int mostVotedQuestionId = getMostVotedQuestion(array);
+                 System.out.print(getQuestionSnippet(array.get(mostVotedQuestionId)));
+            }
+         }
+         else
+            return "sorry, that is not my business";
+
+         return null;
      }
-                   //getting questions for the user
-//                   URL url = new URL(API_PREFIX+"users/633980/questions?key="+API_KEY);
-//                   url.openStream();
-//                   String output = (String)url.getContent();
-//                   System.out.println(output);
-//                   BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
-//                   final JsonElement jsonElement = parser.parse(new InputStreamReader(url.openStream()));
-//                   final JsonObject jsonObject = jsonElement.getAsJsonObject();
-//                   String inputLine;
-//                   while ((inputLine = in.readLine()) != null)
-//                        System.out.println(inputLine);
-
-//                   for (final Map.Entry<String, JsonElement> entry : jsonObject.entrySet()) {
-//                        final String key = entry.getKey();
-//                        final JsonElement value = entry.getValue();
-//                        System.out.println(value.toString());
-//                   }
 
 
-     public String fetch (String s) {
-         return "fetched ";
+    /**
+     * fetch the reponse, ungzipping the stream and returning json object
+     *
+     * @param url
+     * @return
+     */
+    private JsonObject fetch(String url) {
+        final JsonParser parser = new JsonParser();
+        final HttpClient httpClient = new DefaultHttpClient();
+
+        try {
+            HttpGet request = new HttpGet(url);
+            request.addHeader("Accept-Encoding", "gzip");
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                GZIPInputStream inputStream = new GZIPInputStream(entity.getContent());
+                final JsonElement jsonElement = parser.parse(new InputStreamReader(inputStream));
+                final JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+                return jsonObject;
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * getting most voted question
+     * @param
+     * @return
+     */
+     private int getMostVotedQuestion(JsonArray array) {
+
+         int idOfMax = 0;
+         int maxVote = 0;
+         int j=0;
+         Iterator<JsonElement> iterator = array.iterator();
+
+         while (iterator.hasNext()) {
+             JsonElement currentVote= iterator.next().getAsJsonObject().get("up_vote_count");
+             if (Integer.parseInt(currentVote.toString())>maxVote) {
+                 maxVote = Integer.parseInt(currentVote.toString());
+                 idOfMax = j;
+             }
+             j++;
+         }
+
+         return idOfMax;
      }
+
+
+    private String getTheBestAnswer (String id) {
+        //TODO
+         return null;
+    }
+
+
+    /**
+     * get question presentation
+     * @param element
+     * @return
+     */
+    private String getQuestionSnippet (JsonElement element) {
+        StringBuffer result = new StringBuffer();
+        String title= element.getAsJsonObject().get("title").toString();
+        result.append("Question with title: "+title+"\n");
+        return result.toString();
+    }
+
+
+
 }
